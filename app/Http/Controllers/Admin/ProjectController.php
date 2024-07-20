7bec731220da081d6e9d8a44eaad07528f813293
antoniozacca <antoniozaccaria057@gmail.com>
2024-07-20 18:09:40 +0200

    feat: add projects in projectcontroller

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -13,7 +13,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        return view('admin.project');
+        $project = Project::all();
+        return view('admin.project', compact('project'));
     }
 
     /**
