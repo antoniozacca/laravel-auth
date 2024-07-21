8d350c65b06a1a8760409ca57161a49caebf46cd
antoniozacca <antoniozaccaria057@gmail.com>
2024-07-21 18:39:02 +0200

    feat: add project in page

--- resources/views/admin/project.blade.php
+++ resources/views/admin/project.blade.php
@@ -1,34 +1,40 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
+      <div class="header-page d-flex justify-content-between align-items-center">
+
         <h1>Lista dei progetti</h1>
+        <div>
+          <button class="btn btn-primary">Crea nuovo progetto</button>
+        </div>
+      </div>
         <table class="table">
             <thead>
               <tr>
                 <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">Title</th>
+                <th scope="col">Content</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Edit</th>
               </tr>
             </thead>
             <tbody>
+              @foreach ($project as $i)
+                
               <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
+                <th scope="row">{{$i->id}}</th>
+                <td>{{$i->title}}</td>
+                <td>{{$i->content}}</td>
+                <td>{{$i->slug}}</td>
+                <td>
+                  <div class="d-flex gap-2">
+                    <a href="#" class="btn btn-info" as="button"><i class="fab fa-searchengin"></i></a>
+                    <a href="#" class="btn btn-warning" as="button"><i class="fas fa-pencil"></i></a>
+                    <a href="#" class="btn btn-danger" as="button"><i class="fas fa-bomb"></i></a>
+                  </div>
+                </td>
               </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
+              @endforeach
             </tbody>
           </table>
     </div>
